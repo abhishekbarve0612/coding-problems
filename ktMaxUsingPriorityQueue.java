2c1b3a36300573fb9357d6f70a8045d5b2d93dad
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-02-15 15:30:34 +0530

    Revised Heap Implementation Usin

---
+++ ktMaxUsingPriorityQueue.java
@@ -0,0 +1,21 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class ktMaxUsingPriorityQueue {
+  public static void main(String[] args) {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    int arr[] = new int[] { 2, 43, 232, 45, 232, 234, 323, 45, 23 };
+    int k = 2;
+    for (int i : arr) {
+      maxHeap.add(i);
+      minHeap.add(i);
+    }
+    while (k-- > 0) {
+      maxHeap.remove();
+      minHeap.remove();
+    }
+    System.out.print(maxHeap.peek() + "  " + minHeap.peek());
+  }
+}
